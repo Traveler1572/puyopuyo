de3c437bc0f14123ca538a90b8b2806d37cb1570
Traveler1572 <all_over_the_world2003@hotmail.com>
2021-04-12 15:15:56 +0000

    stage.js

---
+++ stage.js
@@ -0,0 +1,12 @@
+class Stage {
+    // static stageElement;
+    // static scoreElement;
+    // static zenkeshiImage;
+    // static board;
+    // static puyoCount;
+    // static fallingPuyoList = [];
+    // static eraseStartFrame;
+    // static erasingPuyoInfoList = [];
+
+    
+}
