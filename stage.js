5c3f962d746fd39ee872abf89e6dcf67e7d8232c
Traveler1572 <all_over_the_world2003@hotmail.com>
2021-04-14 14:47:29 +0000

    stage.js

--- stage.js
+++ stage.js
@@ -8,5 +8,57 @@ class Stage {
     // static eraseStartFrame;
     // static erasingPuyoInfoList = [];
 
-    
+    static initialize() {
+        // HTMLからステージの元となる要素を取得し、大きさを設定する
+        const stageElement = document.getElementById("stage");
+        stageElement.style.width = Config.puyoImgWidth * Config.stageCols + 'px';
+        stageElement.style.height = Config.puyoImgHeight * Config.stageRows + 'px';
+        stageElement.style.backgroundColor = Config.stageBackgroundColor;
+        this.stageElement = stageElement;
+
+        const zenkeshiImage = document.getElementById("zenkeshi");
+        zenkeshiImage.width = Config.puyoImgWidth * 6;
+        zenkeshiImage.style.position = 'absolute';
+        zenkeshiImage.style.display = 'none';
+        this.zenkeshiImage = zenkeshiImage;
+        stageElement.appendChild(zenkeshiImage);
+
+        const scoreElement = document.getElementById("score");
+        scoreElement.style.backgroundColor = Config.scoreBackgroundColor;
+        scoreElement.style.top = Config.puyoImgHeight * Config.stageRows + 'px';
+        scoreElement.style.width = Config.puyoImgWidth * Config.stageCols + 'px';
+        scoreElement.style.height = Config.fontHeight + "px";
+        this.scoreElement = scoreElement;
+
+        // メモリを準備する
+        this.board = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+        let puyoCount = 0;
+        for(let y = 0; y < Config.stageRows; y++) {
+            const line = this.board[y] || (this.board[y] = []);
+            for(let x = 0; x < Config.stageCols; x++) {
+                const puyo = line[x];
+                if (puyo >= 1 && puyo <= 5) {
+                    // line[x] = {puyo: puyo, element: this.setPuyo(x, y, puyo)};
+                    this.setPuyo(x, y, puyo);
+                    puyoCount++;
+                } else {
+                    line[x] = null;
+                }
+            }
+        }
+        this.puyoCount = puyoCount;
+    }
 }
