a548c6da74d37680455059f5169c704611246119
Traveler1572 <all_over_the_world2003@hotmail.com>
2021-04-20 14:07:47 +0000

    stage.js

--- stage.js
+++ stage.js
@@ -138,5 +138,51 @@ class Stage {
         return isFalling;
     }
 
-    
+    // 消せるかどうか判定する
+    static checkErase(startFrame) {
+        this.eraseStartFrame = startFrame;
+        this.erasingPuyoInfoList.length = 0;
+
+        // 何色のぷよを消したかを記録する
+        const erasePuyoColor = {};
+
+        // 隣接ぷよを確認する関数内関数を作成
+        const sequencePuyoInfoList = [];
+        const existingPuyoInfoList = [];
+        const checkSequentialPuyo = (x, y) => {
+            // ぷよがあるか確認する
+            const orig = this.board[y][x];
+            if (!orig) {
+                // ないなら何もしない
+                return;
+            }
+            // あるなら一旦退避して、メモリ上から消す
+            const puyo = this.board[y][x].puyo;
+            sequencePuyoInfoList.push({
+                x: x,
+                y: y,
+                cell: this.board[y][x]
+            });
+            this.board[y][x] = null;
+
+            // 四方向の周囲ぷよを確認する
+            const direction = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+            for(let i = 0; i < direction.length; i++) {
+                const dx = x + direction[i][0];
+                const dy = y + direction[i][1];
+                if (dx < 0 || dy < 0 || dx >= Config.stageCols || dy >= Config.stageRows) {
+                    // ステージの外にはみ出た
+                    continue;
+                }
+                const cell = this.board[dy][dx];
+                if (!cell || cell.puyo !== puyo) {
+                    // ぷよの色が違う
+                    continue;
+                }
+                // そのぷよのまわりのぷよも消せるか確認する
+                checkSequentialPuyo(dx, dy);
+            };
+        };
+        
+    }
 }
